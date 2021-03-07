0fead46a5b905a01a6a17b675b4ce9403294a337
cparros <celparros@gmail.com>
2021-03-07 17:57:19 -0500

    fixing connection. can render burgers table on consoel perfectly and server connects

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,12 @@
 const connection = require('./connection')
 
 const orm = {
-  selectALL(tableInput, cb) {
-    const queryString = 'SELECT * FROM ??';
+  selectALL(tableInput) {
+    const queryString = `SELECT * FROM ${tableInput}`;
     connection.query(
       queryString, [tableInput], (err, res) => {
         if(err) throw err;
-        cb(res)
+        console.table(res)
       }
     )
   }
--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const express = require('express')
-const hdlbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers/burger_controller');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
