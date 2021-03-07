c919cedd75765a5ae4866e8e3b3b8627f1f7428d
cparros <celparros@gmail.com>
2021-03-07 17:15:42 -0500

    added seeds and scheama sql files in db folder

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('All-american', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowboy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Soldier', false);
