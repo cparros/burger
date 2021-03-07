b5e0ba8643c02e2f9cb028fa5542beb3937ef620
cparros <celparros@gmail.com>
2021-03-07 17:31:28 -0500

    imported orm to burger js in newly created models folder. orm only has select all funcitonality now

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,15 @@
 const connection = require('./connection')
+
+const orm = {
+  selectALL(tableInput, cb) {
+    const queryString = 'SELECT * FROM burgers';
+    connection.query(
+      queryString, [tableInput], (err, res) => {
+        if(err) throw err;
+        cb(res)
+      }
+    )
+  }
+}
+
+module.exports = orm;
